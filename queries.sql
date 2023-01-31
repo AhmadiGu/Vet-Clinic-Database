e938e8eb5098685ee73028b39da7957aecda4175
AhmadiGu <ahmadigulalai76@gmail.com>
2023-01-31 17:08:38 +0500

    Write queries that spasified in requierment

--- queries.sql
+++ queries.sql
@@ -14,3 +14,63 @@ SELECT * FROM animals WHERE neutered IS true;
 SELECT * FROM animals WHERE name NOT LIKE 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+-- New Queries T1
+BEGIN; 
+UPDATE animals SET  species = 'unspecified'; 
+SELECT species FROM animals;
+ROLLBACK;
+
+SELECT species FROM animals;
+
+-- T2
+BEGIN; 
+UPDATE animals SET  species = 'digimon' 
+WHERE name LIKE '%mon%'; 
+
+UPDATE animals SET  species = 'pokemon' 
+WHERE species is null ;
+SELECT species FROM animals; 
+COMMIT; 
+
+SELECT species FROM animals;
+
+--T3
+SELECT * FROM animals; 
+BEGIN;  
+DELETE from animals;
+ROLLBACK;  
+SELECT * FROM animals;
+
+--T4
+
+BEGIN; 
+
+DELETE from animals 
+WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT birth;
+
+UPDATE animals
+SET weight_kg = -weight_kg;
+ROLLBACK TO birth; 
+
+UPDATE animals
+SET weight_kg = -weight_kg
+WHERE  weight_kg <0;
+
+COMMIT; 
+ 
+SELECT * FROM animals;
+
+-- The calculate
+ 
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg)
+ FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE
+ date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+SELECT species, CAST(AVG(escape_attempts) AS DECIMAL(10)) FROM animals 
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
