1c529395a6446c7e4e17d957ff4358dd31e8f4c3
AhmadiGu <ahmadigulalai76@gmail.com>
2023-02-01 17:13:56 +0500

    Create species, owners tables and modify animals table

--- schema.sql
+++ schema.sql
@@ -13,3 +13,36 @@ CREATE TABLE animals (
 
 ALTER TABLE animals 
 ADD species varchar(200);
+
+-- New Tables
+
+CREATE TABLE owners (
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  full_name varchar(250),
+  age INT
+);
+
+CREATE TABLE species (
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name varchar(150)
+   
+);
+
+
+ALTER TABLE animals
+ DROP COLUMN id;
+ALTER TABLE animals ADD COLUMN
+ id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY;
+
+ALTER TABLE animals
+ DROP COLUMN species;
+
+ALTER TABLE animals
+ ADD COLUMN species_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_species 
+FOREIGN KEY(species_id) REFERENCES species(id);
+
+ALTER TABLE animals 
+ ADD COLUMN owner_id INT; 
+ALTER TABLE animals ADD CONSTRAINT fk_owners 
+FOREIGN KEY(owner_id) REFERENCES owners(id);
