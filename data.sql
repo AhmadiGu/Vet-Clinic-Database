6e3ef5eda53d2a4181bc988b59c52ab76884c202
AhmadiGu <ahmadigulalai76@gmail.com>
2023-02-06 14:53:30 +0500

    insert some data

--- data.sql
+++ data.sql
@@ -116,6 +116,7 @@ INSERT INTO visits (vets_id, animals_id, date_of_visit) VALUES( (SELECT id FROM 
 
 INSERT INTO visits (vets_id, animals_id, date_of_visit) VALUES( (SELECT id FROM vets WHERE name = 'William Tatcher'), (SELECT id FROM animals WHERE name = 'Blossom'), 'Jan 11, 2021');
 
+
 -- NEW DATA 
 INSERT INTO visits (animals_id, vets_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animals_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
 insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
